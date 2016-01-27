eb4f60de14691bedb393ef4594f766ec6716192f
Sara Randolph <sara.randolph222@gmail.com>
2016-01-27 15:43:30 -0700

    player draws to screen, starts with no picture

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,39 @@
-var players = {};
-var grabInput = $('player-roster')
+var playerRoster = [];
+var form = $('form')
 
-$('#addPlayer').submit(function(e){
+// Player Constructor
+function Player(name, position, playerNumber){
+    this.name = name;
+    this.position = position;
+    this.playerNumber = playerNumber;
+}
+
+// Add Player to playerRoster Object, Calls drawPlayer Function, Resets Form
+function addPlayerToRoster(e){
     
     e.preventDefault();
-    var vm = this;
-    var name = vm['playerName'];
-    var position = vm['playerPosition'];
-    var num = vm['playerNumber']
+    var playerName = $("[name='playerName']").val();
+    var playerPosition = $("[name='playerPosition']").val();
+    var playerNumber = $("[name='playerNumber']").val();
     
-    var newPlayer = new Player(name, position, num);
-    draw(newPlayer);
+    var newPlayer = new Player(playerName, playerPosition, playerNumber);
+    playerRoster.push(newPlayer); 
     
-})
+    drawPlayer(newPlayer);
+ 
+   $("form")[0].reset();
+
+};
+
+// Draws Player to the Page
+function drawPlayer(currentPlayer){
+
+    var whatTheyAddIn = '<div class="player-card"><button>Remove</button> <br><img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" alt="playerimage"/> <p class="player-name">' + currentPlayer.name + '</p> <p class="player-position">' + currentPlayer.position + '</p><pclass="player-number">' + currentPlayer.playerNumber + '</p></div>';
+    $('.player-roster').append(whatTheyAddIn);
+
+}
+
+
+    
+// Calls addPlayer Function
+form.submit(addPlayerToRoster)
